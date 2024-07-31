815036387a2612c035f4617efbe05b754c257729
Guilherme Barrinha <guilhermebarrinhams@gmail.com>
2024-07-31 16:09:50 -0300

    Obtendo um post por id

---
+++ src/main/java/com/guilherme/workshopmongo/resources/PostResource.java
@@ -0,0 +1,26 @@
+package com.guilherme.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.guilherme.workshopmongo.domain.Post;
+import com.guilherme.workshopmongo.dto.UserDTO;
+import com.guilherme.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
