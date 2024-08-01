70efe644a5994f68c418c396febde44fe754d280
Guilherme Barrinha <guilhermebarrinhams@gmail.com>
2024-08-01 17:37:45 -0300

    Consulta com vários critérios

--- src/main/java/com/guilherme/workshopmongo/services/PostService.java
+++ src/main/java/com/guilherme/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.guilherme.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guilherme.workshopmongo.domain.Post;
-import com.guilherme.workshopmongo.domain.User;
 import com.guilherme.workshopmongo.repository.PostRepository;
 import com.guilherme.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,7 +25,10 @@ public class PostService {
 	public List<Post> findByTitle(String text){
 		//return repo.findByTitleContainingIgnoreCase(text);
 		return repo.searchTitle(text);
-		
 	}
 
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	} 
 }
